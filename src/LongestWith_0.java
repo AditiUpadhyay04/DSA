96b6c52a202be1585b8df2e25098c04887c8c287
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-01-25 23:05:40 +0530

    GFG

---
+++ src/LongestWith_0.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+
+public class LongestWith_0 {
+
+        int maxLength(int arr[]) {
+
+            int n = arr.length;
+            int max = 0;
+            int k = 0;
+
+            HashMap<Long , Integer> HMM = new HashMap<>();
+
+            long prefix = 0;
+
+            for(int i = 0; i < n ; i++){
+                prefix += arr[i];
+
+                if(prefix == k){
+                    max = Math.max(max , i + 1);
+                }
+                long rem = prefix - k;
+                if(HMM.containsKey(rem)){
+                    max = Math.max(max , i - HMM.get(rem));
+
+                }
+                if(!HMM.containsKey(prefix)){
+                    HMM.put(prefix , i);
+                }
+
+            }
+            return max;
+
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
