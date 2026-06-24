ea3ae22cd9de92ca68b3a702b09cbd1b56a0642a
Aditi Upadhyay <aditiupadhyay102@gmail.com>
2026-06-24 22:44:42 +0530

    Update Second.java

--- src/Graph/Second.java
+++ src/Graph/Second.java
@@ -3,7 +3,7 @@ package Graph;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Second {
+public class Second{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of vertex:");
